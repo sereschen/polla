52dcb4931a812e42d6ade787570af4ee5b61e0cf
Sergio Escobar <sereschen@gmail.com>
2018-01-25 17:21:32 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const Services = require("./services");
+const services = new Services();
+const app = express();
+
+app.listen(3000, () => console.log("listening on port 3000!"));
+
+app.get("/matches", (req, res) => {
+  console.log("It works");
+  services
+    .getMatches()
+    .then(
+      matches => {
+        console.log(matches);
+        res.send(matches);
+      },
+      error => res.send(error)
+    )
+    .catch(error => console.log(error));
+});
+
+app.get("/teams", (req, res) => {
+  services
+    .getTeams()
+    .then(
+      teams => {
+        console.log(teams);
+        res.send(teams);
+      },
+      error => res.send(error)
+    )
+    .catch(error => console.log(error));
+});
+
+app.get("/ranking", (req, res) => {
+  let users = [];
+  db
+    .collection("users")
+    .find()
+    .toArray((err, items) => {
+      return resolve => {
+        resolve(items);
+      };
+    });
+});
---
+++ services.js
@@ -0,0 +1,42 @@
+module.exports = function Services() {
+  this.getMatches = getMatches;
+  this.getTeams = getTeams;
+};
+
+function Mongo() {
+  return new Promise(resolve => {
+    const MongoClient = require("mongodb").MongoClient;
+    let db = null;
+    MongoClient.connect("mongodb://localhost:27017", (err, database) => {
+      if (err) throw err;
+      db = database.db("polla");
+      resolve(db);
+    });
+  });
+}
+
+const getMatches = () => {
+  return new Promise((resolve, reject) => {
+    Mongo().then(db => {
+      db
+        .collection("matches")
+        .find()
+        .toArray((err, matches) => {
+          !err ? resolve(matches) : reject(err);
+        });
+    });
+  });
+};
+
+const getTeams = () => {
+  return new Promise((resolve, reject) => {
+    Mongo().then(db => {
+      db
+        .collection("teams")
+        .find()
+        .toArray((err, teams) => {
+          !err ? resolve(teams) : reject(err);
+        });
+    });
+  });
+};
